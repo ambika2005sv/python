99b141107c1053fd5dd43623c29d08273555b1c1
ambika2005sv <ambika2005sv@gmail.com>
2024-09-18 11:13:52 +0530

    Add files via upload

---
+++ breadth first search.py
@@ -0,0 +1,17 @@
+graph={
+  'A'=('B','C')
+  'B'=('D')
+  'C'=('E')
+  'D'=[]
+  'E'=('F')
+  'F'=[]
+}   
+visited=[]
+if node not in visited
+   print(node)
+   visited.append(node):
+   visit[node]
+   graph.visited[]
+def bfs(visited,graph,neighbour)
+
+bfs(visited,graph,'A')
